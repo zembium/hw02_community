71f91d0555ea1d30f0b7d5208a8c055511e8c9cc
zembium <mash_masha01@mail.ru>
2022-03-02 23:54:46 +0300

    тут подправила код, поменяла поле slug и сделала full name автора :)

--- yatube/posts/models.py
+++ yatube/posts/models.py
@@ -7,7 +7,7 @@ User = get_user_model()
 
 class Group(models.Model):
     title = models.CharField(max_length=200)
-    slug = models.TextField()
+    slug = models.SlugField()
     description = models.TextField()
 
     def __str__(self):
