91992b10117392cd5a5a350cb745ed204723659b
zembium <mash_masha01@mail.ru>
2022-03-07 00:49:29 +0300

    Можно лучше :)

--- yatube/posts/models.py
+++ yatube/posts/models.py
@@ -1,5 +1,4 @@
 from django.db import models
-# Из модуля auth импортируем функцию get_user_model
 from django.contrib.auth import get_user_model
 
 User = get_user_model()
--- yatube/yatube/urls.py
+++ yatube/yatube/urls.py
@@ -3,6 +3,5 @@ from django.urls import include, path
 
 urlpatterns = [
     path('', include('posts.urls', namespace='posts')),
-    path('group/<slug:slug>/', include('posts.urls', namespace='posts')),
     path('admin/', admin.site.urls),
 ]
